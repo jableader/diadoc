a8dd5f981861bf935a2e291a7158784632bdfa52
jableader <jacobdunk@gmail.com>
2022-03-08 22:10:10 +1100

    Arrow heads

--- client/src/components/svg-layout.vue
+++ client/src/components/svg-layout.vue
@@ -2,6 +2,13 @@
   <svg ref="canvas"
     width="100%"
     height="100%">
+    <defs>
+      <marker id="head" orient="auto"
+        markerWidth="5" markerHeight="6"
+        refX="0.1" refY="2">
+        <path d="M0,0 V4 L5,2 Z" fill="black"/>
+      </marker>
+    </defs>
 
     <g id="svg-pan-zoom-viewport" :class="{ animate: recentering }">
       <svg-box v-for="s in shapes" :shape="s" :key="s.id" @shape-selected="shapeSelected" />
@@ -12,7 +19,8 @@
       <g>
         <line v-for="link in links" :key="link"
           :x1="link.p1.x" :x2="link.p2.x" :y1="link.p1.y" :y2="link.p2.y"
-          stroke="black" strokeWidth="1" strokeOpacity="0.5"/>
+          stroke="black" strokeWidth="1" strokeOpacity="0.5"
+          marker-end='url(#head)'/>
       </g>
     </g>
   </svg>
