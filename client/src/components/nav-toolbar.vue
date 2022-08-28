1e6528151c096bc5bfd0b93735f4ed5a7f8419ee
jableader <jacobdunk@gmail.com>
2022-08-28 12:09:41 +1000

    toolbar tooltips

--- client/src/components/nav-toolbar.vue
+++ client/src/components/nav-toolbar.vue
@@ -1,24 +1,53 @@
 <template>
     <div class="toolbar">
-        <button @click="$emit('recenter')">Recenter</button>
+        <div class="tooltip" v-if="tooltip">{{ tooltip }}</div>
+        <div class="toolbar-components">
+            <button 
+                @mouseenter="ev => showTooltip(ev, 'Recenter the image on screen')" 
+                @mouseleave="hideTooltip"
+                @click="$emit('recenter')">
+                    Recenter
+            </button>
+            <span
+                @mouseenter="ev => showTooltip(ev, 'Adjust speed of transitions')" 
+                @mouseleave="hideTooltip">
+                    <img src="/top-speed-svgrepo-com.svg" />
+
+            </span>
+        </div>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data: function() {
+        return {
+            tooltip: ''
+        }
+    },
+    methods: {
+        showTooltip(ev, tooltip) {
+            console.log(ev);
+            this.tooltip = tooltip;
+        },
+        hideTooltip(ev) {
+            this.tooltip = '';
+        }
+    }
+}
 </script>
 
 
 <style scoped>
+
     .toolbar {
         position: absolute;
         bottom: 0;
         left: 0;
     }
 
-    button {
-        min-width: 24px;
-        min-height: 24px;
+    .toolbar-components {
+        text-align: left;
     }
 
 </style>
