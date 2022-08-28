2f0bcdbd9b89ae1b15de26054dea4b064082a0d5
jableader <jacobdunk@gmail.com>
2022-08-28 13:04:46 +1000

    Better positioning, transition speed controller

--- client/src/components/nav-toolbar.vue
+++ client/src/components/nav-toolbar.vue
@@ -3,16 +3,16 @@
         <div class="tooltip" v-if="tooltip">{{ tooltip }}</div>
         <div class="toolbar-components">
             <button 
-                @mouseenter="ev => showTooltip(ev, 'Recenter the image on screen')" 
+                @mouseenter="ev => showTooltip(ev, 'Recenter the graph on screen')" 
                 @mouseleave="hideTooltip"
                 @click="$emit('recenter')">
                     Recenter
             </button>
-            <span
+            <span class="transitionSpeed"
                 @mouseenter="ev => showTooltip(ev, 'Adjust speed of transitions')" 
                 @mouseleave="hideTooltip">
                     <img src="/top-speed-svgrepo-com.svg" />
-
+                    <input type="number" :value="transitionSpeed" @input="ev => $emit('update-transition-speed', parseInt(ev.target.value))" />
             </span>
         </div>
     </div>
@@ -20,14 +20,20 @@
 
 <script>
 export default {
+    emits: ['update-transition-speed', 'recenter'],
+    props: {
+        transitionSpeed: { 
+            type: Number,
+            required: true
+        }
+    },
     data: function() {
         return {
-            tooltip: ''
+            tooltip: '',
         }
     },
     methods: {
         showTooltip(ev, tooltip) {
-            console.log(ev);
             this.tooltip = tooltip;
         },
         hideTooltip(ev) {
@@ -48,6 +54,20 @@ export default {
 
     .toolbar-components {
         text-align: left;
+        vertical-align: center;
+        height: 32px;
+    }
+
+    .toolbar-components > * {
+        display: inline-block;
+        max-height: 32px;
+        vertical-align: middle;
+        margin-right: 1em;
+    }
+
+    .transitionSpeed > * {
+        vertical-align: middle;
+        max-width: 4em;
     }
 
 </style>
