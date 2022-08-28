e8f92f6e04ad00b3b4ebbdea682113af33e4bdb1
jableader <jacobdunk@gmail.com>
2022-08-28 11:25:35 +1000

    Moved button to toolbar

---
+++ client/src/components/nav-toolbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="toolbar">
+        <button @click="$emit('recenter')">Recenter</button>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+
+<style scoped>
+    .toolbar {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+    }
+
+    button {
+        min-width: 24px;
+        min-height: 24px;
+    }
+
+</style>
--- client/src/components/svg-layout.vue
+++ client/src/components/svg-layout.vue
@@ -1,48 +1,57 @@
 <template>
-  <svg ref="canvas"
-    width="100%"
-    height="100%">
-    <defs>
-      <marker id="head" orient="auto"
-        markerWidth="5" markerHeight="6"
-        refX="5" refY="2">
-        <path d="M0,0 V4 L5,2 Z" fill="black"/>
-      </marker>
-    </defs>
+  <div class="container">
+    <svg ref="canvas"
+      width="100%"
+      height="100%">
+      <defs>
+        <marker id="head" orient="auto"
+          markerWidth="5" markerHeight="6"
+          refX="5" refY="2">
+          <path d="M0,0 V4 L5,2 Z" fill="black"/>
+        </marker>
+      </defs>
 
-    <g id="svg-pan-zoom-viewport" :class="{ animate: recentering }">
-      <svg-box v-for="s in shapes" :shape="s" :key="s.id" @shape-selected="shapeSelected" />
-      
-        <!-- Debug helper - show bounding boxes
-        <rect v-for="b in bboxes" :key="b" :x="b.x" :y="b.y" :width="b.w" :height="b.h" fill-opacity="0" stroke="#FF0000" strokeWidth="0.5" />
-        -->
-      <g>
-        <line v-for="link in links" :key="link"
-          :x1="link.p1.x" :x2="link.p2.x" :y1="link.p1.y" :y2="link.p2.y"
-          stroke="black" strokeWidth="1" strokeOpacity="0.5"
-          marker-end='url(#head)'/>
+      <g id="svg-pan-zoom-viewport" :class="{ animate: recentering }">
+        <svg-box v-for="s in shapes" :shape="s" :key="s.id" @shape-selected="shapeSelected" />
+        
+          <!-- Debug helper - show bounding boxes
+          <rect v-for="b in bboxes" :key="b" :x="b.x" :y="b.y" :width="b.w" :height="b.h" fill-opacity="0" stroke="#FF0000" strokeWidth="0.5" />
+          -->
+        <g>
+          <line v-for="link in links" :key="link"
+            :x1="link.p1.x" :x2="link.p2.x" :y1="link.p1.y" :y2="link.p2.y"
+            stroke="black" strokeWidth="1" strokeOpacity="0.5"
+            marker-end='url(#head)'/>
+        </g>
       </g>
-    </g>
-  </svg>
-  <button @click="recenter(true)">[ ]</button>
+    </svg>
+    <nav-toolbar 
+      @recenter="recenter(true)"
+      />
+  </div>
 </template>
 
 <style scoped>
-  .animate {
-    transition: transform 0.5s ease;
-  }
 
-  button {
+.animate {
+  transition: transform 0.5s ease;
+}
+
+.container {
     position: absolute;
     top: 0;
+    bottom: 0;
+    left: 0;
     right: 0;
-  }
+}
+
 </style>
 
 <script>
 import svgPanZoom from 'svg-pan-zoom'
 import svgBox from './svg-box.vue'
 import Shape from '@/shape.js'
+import NavToolbar from './nav-toolbar.vue';
 
 function createPanZoom(targetElement) {
     const panZoom = svgPanZoom(targetElement, { 
@@ -86,7 +95,7 @@ function centerElement(box, panZoom){
 }
 
 export default {
-  components: { svgBox },
+  components: { svgBox, NavToolbar },
   emits: ['shape-selected'],
   props: {
       shapes: {
@@ -120,6 +129,7 @@ export default {
   },
   mounted() {
     this.recenter();
+    this.$emit('')
   },
   unmounted() {
     this.resetPanZoom()
