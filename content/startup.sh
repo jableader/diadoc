9a54486e0bd7727a8cf06b241731df9e41ca8e12
jableader <jacobdunk@gmail.com>
2022-09-02 20:10:39 +1000

    Index in startup in background

--- content/startup.sh
+++ content/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
-python /code/app/main.py --reference_path "$REFERENCE_PATH" --index_path "$INDEX_PATH"
+if [[ -z "$(ls "$INDEX_PATH" 2> /dev/null)" ]]; then
+  python /code/app/main.py --reference_path "$REFERENCE_PATH" --index_path "$INDEX_PATH" &
+fi
 
 uvicorn app.main:app --host 0.0.0.0 --port 5000
